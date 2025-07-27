75b795dc4b29f29593ccab73a9a7a036988aee44
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2025-07-27 19:52:58 -0300

    Add Serialize impl for all native types

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,5 +1,6 @@
 use crate::error::DeserializationError;
 use crate::value::Value;
+use std::collections::HashMap;
 
 pub trait FromSon: Sized {
     fn from_son(son: Value) -> Result<Self, DeserializationError>;
@@ -13,54 +14,173 @@ pub trait ToSon {
 
 pub trait Serialize: ToSon {}
 
+// String types
+
 impl Serialize for String {}
 impl ToSon for String {
     fn to_son(&self) -> Value {
-        Value::String(self.clone())
+        return Value::String(self.clone());
     }
 }
 
 impl Serialize for &str {}
 impl ToSon for &str {
     fn to_son(&self) -> Value {
-        Value::String(self.to_string())
-    }
-}
-
-impl Serialize for bool {}
-impl ToSon for bool {
-    fn to_son(&self) -> Value {
-        Value::Bool(*self)
-    }
-}
-
-impl Serialize for i32 {}
-impl ToSon for i32 {
-    fn to_son(&self) -> Value {
-        Value::Integer(*self as i128)
-    }
-}
-
-impl Serialize for f64 {}
-impl ToSon for f64 {
-    fn to_son(&self) -> Value {
-        Value::Float(*self)
+        return Value::String(self.to_string());
     }
 }
 
 impl Serialize for char {}
 impl ToSon for char {
     fn to_son(&self) -> Value {
-        Value::Char(*self)
+        return Value::Char(*self);
     }
 }
 
+// Boolean types
+
+impl Serialize for bool {}
+impl ToSon for bool {
+    fn to_son(&self) -> Value {
+        return Value::Bool(*self);
+    }
+}
+
+// Integer types
+
+impl Serialize for i8 {}
+impl ToSon for i8 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for i16 {}
+impl ToSon for i16 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for i32 {}
+impl ToSon for i32 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for i64 {}
+impl ToSon for i64 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for i128 {}
+impl ToSon for i128 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self);
+    }
+}
+
+impl Serialize for isize {}
+impl ToSon for isize {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+// Unsigned integer types
+
+impl Serialize for u8 {}
+impl ToSon for u8 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for u16 {}
+impl ToSon for u16 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for u32 {}
+impl ToSon for u32 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for u64 {}
+impl ToSon for u64 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for u128 {}
+impl ToSon for u128 {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+impl Serialize for usize {}
+impl ToSon for usize {
+    fn to_son(&self) -> Value {
+        return Value::Integer(*self as i128);
+    }
+}
+
+// Floating point types
+
+impl Serialize for f32 {}
+impl ToSon for f32 {
+    fn to_son(&self) -> Value {
+        return Value::Float(*self as f64);
+    }
+}
+
+impl Serialize for f64 {}
+impl ToSon for f64 {
+    fn to_son(&self) -> Value {
+        return Value::Float(*self);
+    }
+}
+
+// Vector
+
+impl<T> Serialize for Vec<T> where T: Serialize {}
 impl<T> ToSon for Vec<T>
 where
-    T: ToSon,
+    T: Serialize,
 {
     fn to_son(&self) -> Value {
-        Value::Array(self.iter().map(|v| v.to_son()).collect())
+        return Value::Array(self.iter().map(|v| v.to_son()).collect());
+    }
+}
+
+// HashMap
+
+impl<K, V> Serialize for HashMap<K, V>
+where
+    K: ToString,
+    V: Serialize,
+{
+}
+impl<K, V> ToSon for HashMap<K, V>
+where
+    K: ToString,
+    V: Serialize,
+{
+    fn to_son(&self) -> Value {
+        let mut hash_map: HashMap<String, Value> = HashMap::new();
+        for (key, value) in self.iter() {
+            hash_map.insert(key.to_string(), value.to_son());
+        }
+        return Value::Object(hash_map);
     }
 }
 
